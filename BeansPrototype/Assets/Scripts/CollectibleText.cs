3489ff9e298820d039690be97f48ccb27092d98b
zac74192738 <zac74192738.13@gmail.com>
2022-02-21 18:30:59 -0500

    Digital Prototype
    Digital Prototype

---
+++ BeansPrototype/Assets/Scripts/CollectibleText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CollectibleText : MonoBehaviour
+{
+    private TMP_Text collectibles;
+    public static int numCollectibles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectibles = GetComponent<TMP_Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        collectibles.text = "x " + numCollectibles;
+    }
+}
